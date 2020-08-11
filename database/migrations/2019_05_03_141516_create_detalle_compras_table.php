24762f287a23752e1e1bf05b3fa0f5e52dfffc90
gonzak1 <gosquiroga@gmail.com>
2020-08-11 20:39:07 -0300

    ajustes

--- database/migrations/2019_05_03_141516_create_detalle_compras_table.php
+++ database/migrations/2019_05_03_141516_create_detalle_compras_table.php
@@ -20,7 +20,7 @@ class CreateDetalleComprasTable extends Migration
             $table->foreign('idcompra')->references('id')->on('compras')->onDelete('cascade');
             $table->integer('idproducto')->unsigned();
             $table->foreign('idproducto')->references('id')->on('productos');
-            $table->ecimal('cantidad', 11, 2);
+            $table->decimal('cantidad', 11, 2);
             $table->decimal('precio', 11, 2);
            
         });
